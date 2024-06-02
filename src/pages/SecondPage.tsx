782cb77863f6e0a62781d94d65fabe3d2e8af544
evamichelle <e.deltl@campus.tu-berlin.de>
2024-06-02 10:16:12 +0200

    use Storage manager

--- src/pages/SecondPage.tsx
+++ src/pages/SecondPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { uploadData } from "aws-amplify/storage";
+
+import { StorageManager } from "@aws-amplify/ui-react-storage";
 import '@aws-amplify/ui-react/styles.css'
 import { AuthUser } from "aws-amplify/auth";
 import { Flex } from "@aws-amplify/ui-react";
@@ -8,18 +9,18 @@ import '@aws-amplify/ui-react/styles.css';
 
 function SecondPage({user} : {user: AuthUser | undefined}) {
 
-    const [file, setFile] = React.useState();
+    // const [file, setFile] = React.useState();
 
-    const handleChange = (event: any) => {
-        setFile(event.target.files[0]);
-    };
+    // const handleChange = (event: any) => {
+    //     setFile(event.target.files[0]);
+    // };
 
   return (
     <Flex margin="20em" alignSelf="center" direction="column">
       <h1>{user?.signInDetails?.loginId}'s todos</h1>
         <div>
-            <input type="file" onChange={handleChange} />
-            <button
+            {  /*<input type="file" onChange={handleChange} />
+           <button
                 onClick={() =>
                     uploadData({
                         path: `audio_files/${file.name}`,
@@ -28,12 +29,14 @@ function SecondPage({user} : {user: AuthUser | undefined}) {
                 }
             >
                 Upload mp3
-            </button>
+            </button> */}
+            <StorageManager
+      acceptedFileTypes={['audio/*']}
+      path="audio_files/"
+      maxFileCount={1}
+      isResumable
+    />
         </div>
-      <div>
-        🥳 
-        <br />
-      </div>
     </Flex>
   );
 }
