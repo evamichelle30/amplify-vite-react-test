d0d94f59891bba05b1a4f0d41f1b000ae0a20dd7
evamichelle <e.deltl@campus.tu-berlin.de>
2024-06-02 10:20:47 +0200

    stupid warning

--- src/pages/SecondPage.tsx
+++ src/pages/SecondPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { StorageManager } from "@aws-amplify/ui-react-storage";
 import '@aws-amplify/ui-react/styles.css'
 import { AuthUser } from "aws-amplify/auth";
